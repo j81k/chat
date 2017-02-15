53327c8876a27e157e5589f443a7045810bd810a
Jai <Jai>
2017-02-15 21:49:52 +0530

    extra things

---
+++ cmd.bat
@@ -0,0 +1,3 @@
+start
+cd \
+cd xampp\htdocs\_tmp\
--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,8 @@
  * Created On   : 2017-02-13 17:36
  */
 
-var userId = localStorage.getItem('userId') || "1" 
+var socket = io.connect()
+,   userId = socket.userId //localStorage.getItem('userId') || "1" 
 ,   init   = function(){
     var H = $(window).height() - ( $('header').height() + $('footer').height() );
     $('#content > div').css({
@@ -14,22 +15,76 @@ var userId = localStorage.getItem('userId') || "1"
     if( userId == '' ) {
         // Show Login
         dialog($('#login-form'));
+    }else {
+        alert("Your Login ID: "+ userId);
     }
 }
 
-function swap(){
-    if( $('#menu-btn').hasClass('active') ) {
-        // Show List
-        $('#plot').slideDown(800);
-        $('#chat').slideUp(800);
+function chat( type ){
+    var type = typeof type == 'undefined' ? 'message' : type;
 
-        $('#menu-btn').removeClass('active fa-times').addClass('fa-bars');
-    }else {
-        // Show Chat
-        $('#plot').slideUp(400);
-        $('#chat').slideDown(400);
+    var $act = $('#plot .cell.active')
+    ,   name = $act.find('.name').text()
+    ,   group= $act.attr('data-group') || "guest";
 
-        $('#menu-btn').addClass('active fa-times').removeClass('fa-bars');
+    if( group != '' ) {
+        console.log('[To] Group: ' + group+' | Name: '+ name );
+        
+        var data = {
+            type : type,
+            to   : {
+                group : group,
+                name  : name  
+            }
+        };
+        
+        if( type == 'message' ) {
+            data.message = $('#msg').val();
+            
+            /*if( data.message == '' ) {
+                return false;
+            }*/
+        }
+
+        // Open Chat
+        socket.emit('chat', data);
+
+        return true;
+    }
+
+    return false;
+
+}
+
+function swap(type){
+    var type = typeof type == 'undefined' ? 'chat' : type;
+    $('.title-bar').removeClass('active').fadeOut();
+    
+    switch(type) {
+        
+        case 'chat' :
+            // Show Chat
+            chat('init');
+            
+            $('#chat-bar .app-title').html($('#plot .cell.active .name').text());
+            $('#chat-bar').addClass('active');
+            $('#plot').slideUp(400);
+            $('#chat-bar, #chat').slideDown(400);
+            
+        
+        break;
+        
+        case 'menu' :
+            // Show Plot
+            $('#plot-bar').addClass('active');
+            $('#chat-bar, #chat').slideUp(400);
+            $('#plot-bar, #plot').slideDown(400);
+            
+        
+        break;
+        
+        default : break;
+        
     }
 }
 
@@ -48,20 +103,36 @@ function dialog($this, toClose){
     
 }
 
-var Chat = function(socket) {
+/*var chatApp = function(socket) {
     return this.socket = socket;
 }
 
-Chat.prototype.sendMsg = function(msg){
+chatApp.prototype.sendMsg = function(msg){
     this.socket.emit('message', {
        'msg'   : msg,  
     });
-}
+}*/
 
 
-var socket = io.connect();
+
 $(document).on('ready', function(){
-    var chatApp = new Chat(socket);
+    //var chatApp = new chatApp(socket);
+    
+    socket.on('chat', function(result){
+        $('footer').append('<div style="color: #1ba;">'+  result.msg +'</div>');;
+        if( result.status ) {
+            var data = result.data;
+            
+            if( data.type == 'broadcast' ) {
+                // Show the broadcast Message
+                $('#chat').append('<div class="chat-msg">'+ data.message +'</div>');
+            }
+            
+        }else {
+            alert("Error: "+result.msg);
+        }
+        
+    })
     
     socket.on('name', function(result){
         if( result.status ) {
@@ -70,6 +141,7 @@ $(document).on('ready', function(){
             
             userId = result.id;
             //window.localStorge.setItem('userId', userId);
+            socket.userId = userId;
             
             // Get users list
             socket.emit('get', {
@@ -92,9 +164,10 @@ $(document).on('ready', function(){
             
             if( data.length > 0 ) {
                 for(var i in data) {
-                    html += '<div class="cell _bc">';
+                    var row = data[i];
+                    html += '<div class="cell _bc" data-group="'+ row['group'] +'">';
                     html +=     '<div><i class="fa fa-user user-img _bg"></i></div>';
-                    html +=     '<div>'+ data[i] +'</div>';
+                    html +=     '<div class="name">'+ row['name'] +'</div>';
                     html += '</div>';
                 }
                 
@@ -111,17 +184,37 @@ $(document).on('ready', function(){
         
     })
     
-    $('#menu-btn').on('click', function(){
-        swap();
+    
+    socket.on('log', function(result){
+        console.log(result);
+        $('footer').append('<div>'+  result.msg +'</div>');
+    })
+    
+    $('.title-bar .ctrl-btn').on('click', function(){
+        
+        if( $('#chat-bar').hasClass('active') ) {
+            // Show Menu
+            swap('menu');
+        }else {
+            swap();
+        }
+        
     });
     
     $('#chat-form form').submit(function(){
-       
-       
-        return false;
+        // Broadcast Chat
+        chat();
     });
     
-    $('#plot .cell').on('click', function(){
+    $(document).on('click', '#plot .cell', function(){
+        var grp = $(this).attr('data-group') || "";
+        
+        if( grp != '' ) {
+            // Establish connection among this Group
+            $('#plot .cell.active').removeClass('active');
+            $(this).addClass('active');
+            swap();
+        }
         
     });
     
